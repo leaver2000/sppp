a23a745a7498f58428d4f933461eb01656b64fb1
Jason Leaver <leaver2000@gmail.com>
2022-07-20 11:51:20 +0000

    added the download archive function

--- sppp/extract/__init__.py
+++ sppp/extract/__init__.py
@@ -1,3 +1,32 @@
-"""extract functions"""
-__all__ = ["ArchiveTrawler"]
+"""extract functions and classes"""
+__all__ = ["ArchiveTrawler", "download_archive_data"]
+import json
+from pathlib import Path
+
+import pandas as pd
+from requests import Session, HTTPError
+
 from sppp.extract.trawler import ArchiveTrawler
+
+
+def download_archive_data(
+    start: str,
+    end: str,
+    freq: str = "2min",
+    outdir: Path = Path("/home/leaver2000/sppp/archive"),
+) -> None:
+    """accepts a daterange and downloads probsevere data from the archive"""
+    template = "https://mtarchive.geol.iastate.edu/%Y/%m/%d/mrms/ncep/ProbSevere/MRMS_PROBSEVERE_%Y%m%d_%H%M00.json"
+    urls = pd.date_range(start=start, end=end, freq=freq).strftime(template)
+    with Session() as session:
+        for url in urls:
+            try:
+                r = session.get(url, stream=True)
+                r.raise_for_status()
+                outfile = outdir / r.url.split("/")[-1]
+
+                with outfile.open("w") as f:
+                    json.dump(r.json(), f, indent=4)
+
+            except (ConnectionError, HTTPError):
+                print("error downloading", url)
