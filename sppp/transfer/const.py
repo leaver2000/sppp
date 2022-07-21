984f6e88d342c25180c17fc5675f9f837cf0d55b
Jason Leaver <leaver2000@gmail.com>
2022-07-21 10:08:39 +0000

    added probsevere pydantic basemodel

--- sppp/transfer/const.py
+++ sppp/transfer/const.py
@@ -1,16 +1,20 @@
 __all__ = ["GridSpace"]
 import numpy as np
-
-
-def mrms_grid(x_res: int = 7000, y_res: int = 3500):
-    x = np.linspace(-129.995, -60.005002000040975, x_res)
-    y = np.linspace(54.995, 20.005000000002696, y_res)
-    return x, y
+import pandas as pd
 
 
 class GridSpace:
-    def __init__(self, **kwargs: int):
-        self.__x, self.__y = mrms_grid(**kwargs)
+    def __init__(
+        self, x_resolution: int = 7000, y_resolution: int = 3500, **kwargs: int
+    ):
+        self.__x = np.linspace(-129.995, -60.005002000040975, x_resolution)
+        self.__y = np.linspace(
+            54.995, 20.005000000002696, y_resolution
+        )  # mrms_grid(**kwargs)
+
+        self.__zeros = pd.DataFrame(
+            columns=np.arange(len(self.__x)), index=np.arange(len(self.__y))
+        ).fillna(0)
 
     @property
     def x(self) -> np.ndarray:
@@ -23,6 +27,25 @@ class GridSpace:
     def position(self, crd: tuple[float, float]) -> tuple[int, int]:
         x, y = crd
         return np.argmin(abs(self.x - x)), np.argmin(abs(self.y - y))
+
+    def zeros_grid(self) -> pd.DataFrame:
+        return self.__zeros.copy()
+
+    def reward_matrix(self, x: int, y: int, box_size: int = 5) -> pd.DataFrame:
+        def reward_map() -> np.ndarray:
+            """creates 11x11 matrix"""
+            a = list(np.linspace(0, 0.25, 5))
+            a2 = np.array(a + [0.5] + list(reversed(a)))
+            return a2 + a2[:, np.newaxis]
+
+        gamefield = (
+            self.__zeros.loc[
+                y - box_size : y + box_size,
+                x - box_size : x + box_size,
+            ].copy()
+            + reward_map()
+        )
+        return gamefield
 
     def min(self) -> tuple[float, float]:
         return self.x.min(), self.y.min()
